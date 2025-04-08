e92862bb68ce53eb3ac35c4d334fb9873c485a1e
sagestub <sagestubbs@gmail.com>
2025-04-08 04:58:57 -0400

    rudimentary ADC working alongside momentary switch for relay

--- ESP32_SumpMonitor.ino
+++ ESP32_SumpMonitor.ino
@@ -1,21 +1,43 @@
 #include "HardwareSerial.h"
-int relayPin = 3;
-int sensingPin = 18;
-int baudRate=115200;
+#include "esp_adc/adc_continuous.h" //continuous mode adc provides faster read
+
+
+const int relayPin = 3;
+const int sensingPin = 17;
+const int pwmPin = 25;            //GPIO pin for pwm output
+const int pwmDutyCycle = 39;     //PWM duty cycle (0-255)
+const int adcPin = 36;
+const int baudRate=250000;
 boolean relayState = 0;
-
-
+long int intBuffer = 0;
+float avg = 0;
+unsigned int count = 0;
 void setup() {
   Serial.begin(baudRate);
   pinMode(relayPin,OUTPUT);
   pinMode(sensingPin,INPUT_PULLUP);
-
+  analogSetAttenuation(ADC_11db);
+  pinMode(adcPin,INPUT);
+//  pinMode(pwmPin,OUTPUT);
+//  analogWrite(pwmPin,pwmDutyCycle);
 }
 
 void loop() {
   controlMotor(isSwitchOn(sensingPin),0,relayPin);
-  delay(10);
+//  intBuffer+=analogRead(adcPin);
+//  count++; 
+//  if(count>=50){
+//    avg = (float(intBuffer/50));
+//    intBuffer=0;
+//    count = 0;
+//  }
+//  if(count%25<3) {
+//    Serial.println(avg);
+//  }
+  Serial.println(analogRead(adcPin));
+  delay(5);
 }
+
 
 boolean isSwitchOn(int pin) {
   return digitalRead(pin);
